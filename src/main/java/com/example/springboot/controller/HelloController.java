573ab817709da796480a60ab30f9f8fd0c61baab
greatStar123 <1061748368@qq.com>
2019-06-13 15:09:33 +0800

    spring boot 集成jsp

--- src/main/java/com/example/springboot/controller/HelloController.java
+++ src/main/java/com/example/springboot/controller/HelloController.java
@@ -4,6 +4,8 @@ import com.example.springboot.pojo.People;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -25,8 +27,15 @@ public class HelloController {
     private People someOne;
 
     @RequestMapping("/boot/hello")
-    public @ResponseBody String hello() {
+    public @ResponseBody
+    String hello() {
 //        return "Hello Spring Boot! My name is "+name+" and I'm "+age+" years old! ";
-        return "Hello Spring Boot! My name is "+someOne.getName()+" and I'm "+someOne.getAge()+" years old ! ";
+        return "Hello Spring Boot! My name is " + someOne.getName() + " and I'm " + someOne.getAge() + " years old ! ";
+    }
+
+    @GetMapping("/boot/index")
+    public String index(Model model) {
+        model.addAttribute("msg", "Spring Boot 集成 JSP ");
+        return "index";
     }
 }
