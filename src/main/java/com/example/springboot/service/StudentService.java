a42fff66a2a5e1b03ed26da382a396b9a7d5edd6
greatStar123 <1061748368@qq.com>
2019-06-24 17:29:38 +0800

    更改路径

--- src/main/java/com/example/springboot/service/StudentService.java
+++
@@ -1,9 +0,0 @@
-package com.example.springboot.service;
-
-import com.example.springboot.model.Student;
-
-import java.util.List;
-
-public interface StudentService {
-    public List<Student> getAllStudent();
-}
